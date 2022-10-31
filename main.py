2e85e4e44f730a07cb6eb0ccc8d5956b40e93923
edelasoledad-tip <qepdelasoledad@tip.edu.ph>
2022-10-31 15:35:45 +0800

    Click Functions and CSV data integration
    Added click function and CSV data for Search and Cart scroll items

--- main.py
+++ main.py
@@ -14,22 +14,69 @@ LabelBase.register(name = "Nunito", fn_regular= "RES/BebasNeue-Regular.otf")
 
 class ScrollCard(MDCard):
     pass
-    
+class ScrollCardCart(MDCard):
+    pass
 class EasyWareApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.screen = Builder.load_file('KV/main.kv')
-        with open("RES/item_details.csv", 'r') as f:
-            dict_reader = DictReader(f)
-            self.sample = list(dict_reader)
         
-        for x in self.sample:
+        self.cart = [{'name':'Allen Key',
+                      'price':'320.5',
+                      'image':'RES/RES/allenKey.jpg',
+                      'info':'Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Lorem ipsum Dolor sit amet Sed ut perspiciatis',
+                      'stocks':'42',
+                      'brand':'BondHus'},
+                     {'name':'Band Clamp',
+                      'price':'793.1',
+                      'image':'RES/RES/bandClamp.jpg',
+                      'info':'Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Lorem ipsum Dolor sit amet Sed ut perspiciatis',
+                      'stocks':'44',
+                      'brand':'Generic'},
+                     {'name':'Cable Cutter',
+                      'price':'119.5',
+                      'image':'RES/RES/cableCutter.jpg',
+                      'info':'Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Lorem ipsum Dolor sit amet Sed ut perspiciatis',
+                      'stocks':'35',
+                      'brand':'Stanley'},
+                     {'name':'Bricks',
+                      'price':'10.5',
+                      'image':'RES/RES/bricks.jpg',
+                      'info':'Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Lorem ipsum Dolor sit amet Sed ut perspiciatis',
+                      'stocks':'6',
+                      'brand':'ABC'},
+                     {'name':'Cement',
+                      'price':'227.8',
+                      'image':'RES/RES/cement.jpg',
+                      'info':'Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Lorem ipsum Dolor sit amet Sed ut perspiciatis',
+                      'stocks':'19',
+                      'brand':'Generic'}]
+        
+        with open("RES/item_details.csv", 'r') as f:
+            self.home = list(DictReader(f))
+        with open("RES/item_details.csv", 'r') as g:
+            self.search = list(DictReader(g))
+        
+        for x in self.home:
             self.card = ScrollCard()
-            self.card.ids.homename.text = x["'name'"].replace("'", "")
-            self.card.ids.homeprice.text = x["'price'"].replace("'", "")
-            self.card.ids.homeimage.source = x["'image'"].replace("'", "")
+            self.card.ids.homename.text = x['name']
+            self.card.ids.homeprice.text = x['price']
+            self.card.ids.homeimage.source = x['image']
             self.screen.ids.home.ids.homescrollgrid.add_widget(self.card)
-            
+        
+        for y in self.cart:
+            self.cartcard = ScrollCardCart()
+            self.cartcard.ids.cartname.text = y['name']
+            self.cartcard.ids.cartprice.text = y['price']
+            self.cartcard.ids.cartimage.source = y['image']
+            self.screen.ids.cart.ids.cartscrollgrid.add_widget(self.cartcard)
+        
+        for z in self.search:
+            self.searchcard = ScrollCard()
+            self.searchcard.ids.homename.text =z['name']
+            self.searchcard.ids.homeprice.text = z['price']
+            self.searchcard.ids.homeimage.source = z['image']
+            self.screen.ids.search.ids.searchscrollgrid.add_widget(self.searchcard)
         
     def login(self):
         print(self.screen.ids.login.ids.username.text)
@@ -38,11 +85,11 @@ class EasyWareApp(MDApp):
         self.screen.ids.nav_id.ids.nav_drawer.set_state("open")
     
     def temprint(self,text):
-        for x in self.sample:
-            if x["'name'"].replace("'", "") == text:
+        for x in self.home:
+            if x['name'] == text:
                 print(f"Pressed: {text}")
-                print("INFO: ",x["'info'"].replace("'", ""))
-                print("BRAND: ",x["'brand'"].replace("'", ""))
+                print("INFO: ",x['info'])
+                print("BRAND: ",x['brand'])
                 
     def build(self):
         return self.screen  
