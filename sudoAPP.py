3986b617ea167c29d4ffb0783394c598b7adb136
zkmcruz-tip <90205501+zkmcruz-tip@users.noreply.github.com>
2022-11-24 19:22:46 +0800

    Addid testings

---
+++ sudoAPP.py
@@ -0,0 +1,345 @@
+import firebase_admin
+from firebase_admin import credentials, db
+import json
+import requests
+from csv import DictReader
+from traceback import format_list
+
+databaseURL = "https://easywareph-2f473-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+
+class FireDataBase():
+    def __init__(self):
+        cred = credentials.Certificate("keyServiceAccount.json")
+
+        firebase_admin.initialize_app(
+            cred, {'databaseURL': databaseURL})
+
+    def addUser(self, accountType, username, password, fullName):
+        # Included user mode (Admin 1  or Employee 0)
+        try:
+            ref = db.reference('/users')
+            x = ref.get(False, True)
+            items = []
+            for i in x.keys():
+                items.append(i)
+            if username in items:
+                return False
+            else:
+                ref = db.reference(f'/users/{username}')
+                content = {
+                    "username": username,
+                    "accountType": accountType,
+                    "password": password,
+                    "fullName": fullName,
+                    "cart": ""}
+                x = ref.set(content)
+                return True
+        except:
+            ref = db.reference(f'/users/{username}')
+            content = {
+                "username": username,
+                "accountType": accountType,
+                "password": password,
+                "fullName": fullName,
+                "cart": ""}
+            x = ref.set(content)
+            return True
+
+    def getCart(self, username):
+        try:
+            ref = db.reference(f'/users/{username}/cart')
+            cart = ref.get()
+            if cart:
+                return cart
+            else:
+                return False
+        except:
+            return False
+
+    def delCart(self, username, itemID=0):
+        try:
+            ref = db.reference('/users')
+            x = ref.get(False, True)
+            users = []
+            for i in x.keys():
+                users.append(i)
+            if username in users:
+                ref = db.reference(f'/users/{username}/')
+                data = ref.get()
+                cart = data['cart']
+                if not cart:
+                    return False  # Cart already empty
+                else:
+                    if itemID == 0:
+                        ref = db.reference(f'/users/{username}/cart')
+                        ref.set("")
+                        return True  # User Cart emptied
+                    else:
+                        for i in cart:
+                            if i['itemID'] == itemID:
+                                cart.remove(i)
+                        ref = db.reference(f'/users/{username}/cart')
+                        ref.set(cart)
+                        return True  # Cart removed specific
+            else:
+                return False  # User nonexistent
+        except:
+            return False  # DB empty
+
+    def updateCartItemQty(self, username, itemID, quantity):
+        try:
+            ref = db.reference('/users')
+            x = ref.get(False, True)
+            users = []
+            for i in x.keys():
+                users.append(i)
+            if username in users:
+                cart = self.getCart(username)
+                itemList = []
+                for i in cart:
+                    itemList.append(i['itemID'])
+                if itemID in itemList:
+                    for i in cart:
+                        if i['itemID'] == itemID:
+                            if i['quantity'] == quantity:
+                                return False  # Duplicated entry
+                            else:
+                                i['quantity'] = quantity
+                    ref = db.reference(f'/users/{username}/cart')
+                    ref.set(cart)
+                    return True  # Quantity edited
+                else:
+                    return False  # Item not in cart
+            else:
+                return False  # User nonexistent
+        except:
+            return False  # DB empty
+
+    def addToCart(self, username, itemID):
+        try:
+            ref = db.reference('/users')
+            x = ref.get(False, True)
+            users = []
+            for i in x.keys():
+                users.append(i)
+            if username in users:
+                ref = db.reference(f'/users/{username}/cart')
+                cart = ref.get()
+                if not cart:
+                    cart = []
+                    cart.append({
+                        "itemID": itemID,
+                        "quantity": 1})
+                    ref.set(cart)
+                    return True
+                else:
+                    cart = self.getCart(username)
+                    itemList = []
+                    for i in cart:
+                        itemList.append(i['itemID'])
+                    if itemID not in itemList:
+                        cart.append({
+                            "itemID": itemID,
+                            "quantity": 1})
+                        ref.set(cart)
+                        return True
+                    else:
+                        for i in cart:
+                            if i['itemID'] == itemID:
+                                i['quantity'] += 1
+                        ref.set(cart)
+                        return True
+        except:
+            return False
+
+    def testing(self):
+        pass
+
+    def getUser(self, username=''):
+        try:
+            ref = db.reference('/users')
+            x = ref.get(False, True)
+            users = []
+            for i in x.keys():
+                users.append(i)
+            if username == '':
+                userList = []
+                ref = db.reference('/users/')
+                x = ref.get(False, False)
+                for user in x:
+                    userList.append(x[user])
+                return userList
+            else:
+                ref = db.reference(f'/users/{username}')
+                result = ref.get()
+                if result:
+                    return result
+                else:
+                    return False
+        except:
+            return False
+
+    def authLogin(self, username, password):
+        try:
+            ref = db.reference('/users')
+            x = ref.get(False, True)
+            users = []
+            for i in x.keys():
+                users.append(i)
+            if username in users:
+                ref = db.reference(f'/users/{username}')
+                correctPassword = ref.get()['password']
+                if password == correctPassword:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        except:
+            return False
+
+    def updateUser(self, username, mode, newData):  # , mode, content
+        # Mode = what will be updated
+        # 1 = password, 2 = fullName 3 = accountType
+        # It's not allowed to change username.
+        try:
+            ref = db.reference('/users')
+            x = ref.get(False, True)
+            users = []
+            for i in x.keys():
+                users.append(i)
+            if username in users:
+                userData = db.reference(f'/users/{username}').get()
+                if mode == 1:
+                    if newData != userData['password']:
+                        ref = db.reference(f'/users/{username}/password')
+                        ref.set(newData)
+                        return True
+                    else:
+                        return False
+                if mode == 2:
+                    if newData != userData['fullName']:
+                        ref = db.reference(f'/users/{username}/fullName')
+                        ref.set(newData)
+                        return True
+                    else:
+                        return False
+                if mode == 3:
+                    if newData != userData['accountType']:
+                        ref = db.reference(f'/users/{username}/accountType')
+                        ref.set(newData)
+                        return True
+                    else:
+                        return False
+            else:
+                return False
+        except:
+            return False
+
+    def delUser(self, username):
+        try:
+            ref = db.reference('/users')
+            x = ref.get(False, False)
+            users = []
+            for i in x.keys():
+                users.append(i)
+            if username in users:
+                ref = db.reference(f'/users/{username}')
+                ref.delete()
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def getNewItemID(self):
+        ref = db.reference('/items')
+        x = ref.get(False, True)
+        items = []
+        for i in x.keys():
+            items.append(int(i))
+        lastNumer = sorted(items)[-1] + 1
+        return lastNumer
+
+    def createItem(self, itemID, ITEM):
+        if itemID == 0:
+            itemID += 1  # ItemID 0 is reserved for getAll
+        items = []
+        try:
+            ref = db.reference('/items')
+            x = ref.get(False, True)
+            for i in x.keys():
+                items.append(int(i))
+        except:
+            pass
+        if itemID in items:
+            print(f"Item {itemID} already exists")
+            return False
+        else:
+            ref = db.reference(f'/items/{itemID}')
+            ITEM['itemID'] = itemID  # Added itemID val
+            ref.set(ITEM)
+            return True
+
+    def readItem(self, ID=0):
+        if ID == 0:  # Read All items
+            try:
+                ref = db.reference('/items')
+                x = ref.get(False, True)
+                items = []
+                for i in x.keys():
+                    items.append(int(i))
+                ref = db.reference('/items/')
+                x = ref.get(False, False)
+                x.pop(0)
+                return x
+            except:
+                return False  # Returns False if db is empty/does not exist
+        else:  # Read single item
+            ref = db.reference(f'/items/{ID}')
+            temp = ref.get()
+            if temp:
+                return temp
+            else:
+                return False  # Nonexistent
+
+    def updateItem(self, ID, ITEM):
+        try:
+            ref = db.reference('/items')
+            x = ref.get(False, True)
+            items = []
+            for i in x.keys():
+                items.append(int(i))
+            if ID in items:
+                ref = db.reference(f'/items/{ID}')
+                oldItem = ref.get()
+                if oldItem['name'] == ITEM['name'] and oldItem['price'] == ITEM['price']:
+                    return False
+                ref.set(ITEM)
+                return True
+            else:
+                return False  # Item ID does not exist
+        except:
+            return False  # Returns False if db is empty/does not exist
+
+    def deleteItem(self, ID):
+        try:
+            ref = db.reference('/items')
+            x = ref.get(False, True)
+            items = []
+            for i in x.keys():
+                items.append(int(i))
+            if ID in items:
+                ref = db.reference(f'/items/{ID}')
+                ref.delete()
+                return True
+            else:
+                return False
+        except:
+            return False
+
+
+if __name__ == '__main__':
+    app = FireDataBase()
+    app.deleteItem("1")
---
+++ sudoTest.py
@@ -0,0 +1,93 @@
+from sudoAPP import FireDataBase
+from csv import DictReader
+
+
+def populateDatabase(MAX=1001):
+    with open('RES/item_details.csv', 'r') as dataFile:
+        i = 1
+        temp = DictReader(dataFile)
+        sample = list(temp)
+        newDict = {}
+        for data in sample:
+            for entry in data:
+                newDict[entry.replace("'", "")] = data[entry].replace("'", "")
+                newDict['itemID'] = i
+            print(app.createItem(i, newDict))
+            i += 1
+            if i == MAX+1:
+                break
+
+
+sampleItem = {"name": "Allen Key", "price": 320.5, "stocks": 42,
+              "image": "RES/RES/allenKey.jpg", "info": "lorem ips", "brand": "BondHus"}
+editedItem = {"name": "qweqweqwe", "price": 320.5, "stocks": 42,
+              "image": "RES/RES/allenKey.jpg", "info": "lorem ips", "brand": "BondHus"}
+sampleUser1 = 1, "Erickson", "123", "Erickson Dela Soledad"
+sampleUser2 = 0, "ABSON", "123", "Erickson Dela Soledad"
+sampleUser3 = 1, "RONELG", "123", "Erickson Dela Soledad"
+sampleUserUpdate1 = None
+if __name__ == '__main__':
+    app = FireDataBase()
+    # populateDatabase()
+    # app.readItem()
+    # app.createItem(1, sampleItem)
+    # app.updateItem(1000, editedItem)
+    # print(app.readItem())
+    # populateDatabase(2)
+    # Example on how to use get next available num
+    # print(app.createItem(app.getNewItemID(), sampleItem))
+    # print(app.addUser(*sampleUser1))
+    # print(app.addUser(*sampleUser2))
+    # print(app.addUser(*sampleUser3))
+    # print(app.getUser("Erickson"))  # Get one user
+    # print(app.getUser("WrongInput"))  # Get wrong user
+    # print(app.getUser())  # Get all User in listed form
+    # print(app.authLogin("Erickson", "123"))  # Auth right
+    # print(app.authLogin("Erickson", "333"))  # Auth wrong
+    # print(app.authLogin("Nobody", "Nothing"))  # Auth nonexistent user
+
+    # print(app.addUser(*sampleUser1))
+    # print(app.addUser(*sampleUser2))
+    # print(app.addUser(*sampleUser3))
+    # print(app.updateUser("Nobody", 1, "1222")) # Edit nonexistent user
+    # print(app.updateUser("Erickson", 1, "123"))  # Enter the same password
+    # print(app.updateUser("Erickson", 1, "Ulan123"))  # Enter a new password
+    # Enter the same fullname
+    # print(app.updateUser("Erickson", 2, "Erickson Dela Soledad"))
+    # print(app.updateUser("Erickson", 2, "Ericky Montogomery"))# Enter a new fullname
+    # print(app.updateUser("Erickson", 3, 1))  # Enter same account Type
+    # print(app.updateUser("Erickson", 3, 0))  # Enter different account Type
+    # print(app.addUser(*sampleUser1))
+    # print(app.addUser(*sampleUser2))
+    # print(app.addUser(*sampleUser3))
+    # print(app.delUser("Erickson"))  # Delete an existing user
+    # print(app.delUser("Nobodyl"))  # Delete a nonexistent user
+    # app.addToCart("ABC", 1)
+    # print(app.getUser("AWEAS"))
+    # print(app.addToCart("Erickson", 1))
+    # print(app.addToCart("Erickson", 2))
+    # print(app.addToCart("Erickson", 194))
+    # print(app.addToCart("Erickson", 205))
+    # print(app.getCart("Erickson"))
+    # print(app.addToCart("RONELG", 1))
+
+    # Del specific item on cart demo
+    # print(app.addUser(*sampleUser1))
+    # print(app.addToCart("Erickson", 1))
+    # print(app.addToCart("Erickson", 2))
+    # print(app.addToCart("Erickson", 194))
+    # print(app.addToCart("Erickson", 205))
+    # print(app.delCart("Erickson", 1))
+
+    # Edit quanity on cart
+    # print(app.addUser(*sampleUser1))
+    # print(app.addUser(*sampleUser3))
+
+    # print(app.addToCart("Erickson", 1))
+    # print(app.addToCart("Erickson", 2))
+    # print(app.addToCart("Erickson", 194))
+    # print(app.addToCart("Erickson", 205))
+    # print(app.updateCartItemQty("Erickson", 205, 102))  # Item not in cart
+    # print(app.updateCartItemQty("Erickson", 2, 502))  # Item not in cart
+    # print(app.updateCartItemQty("Erickson", 2, 502))  # Same data to input
+    # print(app.updateCartItemQty("RONELG", 1, 202)) # Cart empty
